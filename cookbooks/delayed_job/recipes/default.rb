9df0c4b6f7285ee26015da2434f7484cc22216f3
Jonah Williams <jonah@carbonfive.com>
2009-11-30 15:30:18 -0800

    remove dependency on static app name
    remove app specific worker name
    
    use monit support for specifying user
    
    job_runner not delayed_job
    
    clean up monit reload

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -3,41 +3,38 @@
 # Recipe:: default
 #
 
-if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+node[:applications].each do |app_name, data|
+  user = node[:users].first
 
-  # be sure to replace "app_name" with the name of your application.
-  run_for_app("web") do |app_name, data|
+  case node[:instance_role]
+    when "solo", "app", "app_master"
 
-    worker_name = "delayed_job"
-    
-    # The symlink is created in /data/app_name/current/tmp/pids -> /data/app_name/shared/pids, but shared/pids doesn't seem to be?
-    directory "/data/#{app_name}/shared/pids" do
-      owner node[:owner_name]
-      group node[:owner_name]
-      mode 0755
-    end
+      worker_name = "job_runner"
 
-    template "/etc/monit.d/delayed_job_worker.#{app_name}.monitrc" do
-      source "delayed_job_worker.monitrc.erb"
-      #owner node[:owner_name]
-      #group node[:owner_name]
-      owner "root"
-      group "root"
-      mode 0644
-      variables({
-        :app_name => app_name,
-        :user => node[:owner_name],
-        :worker_name => worker_name,
-        :framework_env => node[:environment][:framework_env]
-      })
-    end
+      # The symlink is created in /data/app_name/current/tmp/pids -> /data/app_name/shared/pids, but shared/pids doesn't seem to be?
+      directory "/data/#{app_name}/shared/pids" do
+        owner node[:owner_name]
+        group node[:owner_name]
+        mode 0755
+      end
 
+      template "/etc/monit.d/delayed_job_worker.#{app_name}.monitrc" do
+        source "delayed_job_worker.monitrc.erb"
+        owner user[:username]
+        group user[:username]
+        mode 0644
+        variables({
+                :app_name => app_name,
+                :user => node[:owner_name],
+                :worker_name => worker_name,
+                :framework_env => node[:environment][:framework_env]
+        })
+      end
+
+    # Reload monit to pick up configuration changes 
     bash "monit-reload-restart" do
-       user "root"
-       code "pkill -9 monit && monit"
+      user "root"
+      code "monit reload"
     end
-
   end
-
-
 end
