874a6b3fe8c63a15eb09397bc42bc18ff5e38ebe
Jonah Williams <jonah@carbonfive.com>
2010-03-24 21:05:22 -0700

    handle initial chef runs better; start monit if not running and no longer fail if no old job runners exist to be killed.

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -9,7 +9,7 @@ node[:applications].each do |app_name, data|
   case node[:instance_role]
     when "solo", "app", "app_master"
 
-      worker_name = "job_runner"
+      worker_name = "job_runner" #safer to make this "#{app_name}_job_runner" if the environment might run multiple apps using delayed_job
 
       # The symlink is created in /data/app_name/current/tmp/pids -> /data/app_name/shared/pids, but shared/pids doesn't seem to be?
       directory "/data/#{app_name}/shared/pids" do
@@ -31,16 +31,17 @@ node[:applications].each do |app_name, data|
         })
       end
 
-    # Reload monit to pick up configuration changes 
+    # Reload monit to pick up configuration changes
     bash "monit-reload-restart" do
       user "root"
-      code "monit reload"
-      #restart the job runner in the new environment
-      #code "monit restart #{worker_name}"
+      code "monit reload && monit"
 
       #kill all workers to remove any orphaned workers caused when monit spawns extra processes
       #see https://cloud-support.engineyard.com/discussions/problems/415-monit-restart-doesnt-operate-reliably
-      code "pidof #{worker_name} | xargs kill"
+      code "pidof #{worker_name} | xargs --no-run-if-empty kill"
+      #were the above not a concern we could simply restart the job runner in the new environment
+      #code "monit restart #{worker_name}"
+
     end
   end
 end
