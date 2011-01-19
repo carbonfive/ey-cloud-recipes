548aac8407c552b1268c6031160213ac55c7e857
Jeremy Yun <jeremy@carbonfive.com>
2011-01-18 17:18:31 -0800

    remove unused recipes

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -8,19 +8,3 @@ require_recipe 'paperclip'
 require_recipe 'google_analytics'
 require_recipe 'ssmtp'
 require_recipe 'delayed_job'
-# require_recipe 's3fs'
-
-# uncomment if you want to run couchdb recipe
-# require_recipe "couchdb"
-
-# uncomment to turn use the MBARI ruby patches for decreased memory usage and better thread/continuationi performance
-# require_recipe "mbari-ruby"
-
-# uncomment to turn on thinking sphinx 
-# require_recipe "thinking_sphinx"
-
-# uncomment to turn on ultrasphinx 
-# require_recipe "ultrasphinx"
-
-#uncomment to turn on memcached
-# require_recipe "memcached"
